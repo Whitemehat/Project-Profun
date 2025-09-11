7cfc6f29fb2ab69a71d6e7b03bbfcf9758fe4df8
Whitemehat <wong1234.pe@gmail.com>
2025-09-11 09:13:01 +0700

    lastest

--- project/project.c
+++ project/project.c
@@ -234,39 +234,3 @@ int display_menu()
     scanf("%d" , &option);
     return option;
 }
-
-
-int main()
-{
-    int option;
-    char filename[9] = "data.csv";
-    load_csv(filename);
-    while (1){    
-        option = display_menu();
-        switch (option)
-        {
-            case 1:
-                list_patient();
-                break;
-            case 2:
-                add_patient();
-                save_csv(filename);
-                break;
-            case 3:
-                search();
-                break;
-            case 4:
-                update_patient();
-                save_csv(filename);
-                break;
-            case 5:
-                delete_patient();
-                save_csv(filename);
-                break;
-            default:
-                printf("Invalid input\n");
-        }
-    }
-    
-    return 0;
-}
---
+++ project/project.h
@@ -0,0 +1,27 @@
+#ifndef PROJECT_H
+#define PROJECT_H
+
+#define MAX_LINE 1024
+
+typedef struct{
+    char name[50];
+    int age;
+    char disease[100];
+    char date[20];
+}Patient;
+
+extern Patient *patients;
+extern int patient_count;
+
+int sort_patient(const char *keyword);
+int check_condition(char *condition);
+void load_csv(const char *filename);
+void save_csv(const char *filename);
+void list_patient();
+void add_patient();
+void search();
+void update_patient();
+void delete_patient();
+int display_menu();
+
+#endif
